23bebee6040b03e681cf94a4dafe4227cb173c07
Thudlender <tthunder1688@gmail.com>
2024-07-25 16:13:45 +0700

    middleware/authjwt.js

--- routers/restaurant.router.js
+++ routers/restaurant.router.js
@@ -1,14 +1,29 @@
 const express = require("express");
 const router = express.Router();
 const restaurantController = require("../controllers/restaurant.controllers");
+const {authJwt} = require("../middlewares");
 
-//Craete a restaurant
+//Create a restaurant
 //POST http://localhost:500/api/v1/restaurant
-router.post("/", restaurantController.create);
+router.post("/",[authJwt.verifyToken, authJwt.isModOrAdmin], restaurantController.create);
 
-//Get a restaurant
+//Get All restaurant
 router.get("/", restaurantController.getAll)
 
-router.get("/:id", restaurantController.getByid);
+//Get a restaurant by Id
+router.get("/:id", [authJwt.verifyToken], restaurantController.getByid);
 
+//update a restaurant
+router.put(
+    "/:id",
+    [authJwt.verifyToken, authJwt.isModOrAdmin],
+    restaurantController.update
+);
+
+//delete a restaurant
+router.delete(
+    "/:id",
+    [authJwt.verifyToken, authJwt.isAdmin],
+    restaurantController.delete
+);
 module.exports = router;
